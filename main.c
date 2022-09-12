34c853fc3c21d56a0ad6e9d0d5dfdfb01bba9909
Kasipeti <113339465+Kasipeti@users.noreply.github.com>
2022-09-12 10:24:06 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ( )
+{
+    int n=42;
+    unsigned int u_n= 10;
+    float  f =3.14;
+    double d=3.141592;
+    char c='a';
+
+
+
+   printf( "%d %u %f %f %c\n" , n, u_n, f, d, c);
+   printf("%s","Hello Word\n");
+
+}    
